1e5a5eaa406ad8c6fab82fff1dde98cfd9ef7a1a
Mike Bronner <mike@genealabs.com>
2018-06-10 09:31:50 -0700

    WIP

--- src/Providers/Service.php
+++ src/Providers/Service.php
@@ -15,14 +15,34 @@ class Service extends ServiceProvider
 
     public function register()
     {
-        $this->app->singleton("phrets", function () {
-            $configuration = (new Configuration)
-                ->setLoginUrl(config("services.phrets.login-url"))
-                ->setUsername(config("services.phrets.username"))
-                ->setPassword(config("services.phrets.password"))
-                ->setRetsVersion(config("services.phrets.rets-version"));
+        $this->app->singleton("phrets", function ($app) {
+            $connections = [];
 
-            return new Session($configuration);
+            foreach (config("services.phrets") as $phretsConnection => $config) {
+                $configuration = (new Configuration)
+                    ->setLoginUrl($config["login-url"])
+                    ->setUsername($config["username"])
+                    ->setPassword($config["password"])
+                    ->setHttpAuthenticationMethod($config["authentication-method"])
+                    ->setRetsVersion($config["rets-version"]);
+
+                if (! app()->environment("production")) {
+                    $configuration->setLoginUrl($config["staging-login-url"]);
+                }
+
+                $phrets = new Session($configuration);
+
+                if (config("app.debug") === true) {
+                    $log = new \Monolog\Logger('PHRETS');
+                    $log->pushHandler(new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Logger::DEBUG));
+                    $phrets->setLogger($log);
+                }
+
+                $phrets->Login();
+                $connections[$phretsConnection] = $phrets;
+            }
+
+            return $connections;
         });
     }
 
